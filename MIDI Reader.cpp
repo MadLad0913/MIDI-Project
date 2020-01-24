cc66387137909e14d75a185928f45745c71da6df
MadLad0913 <46514516+MadLad0913@users.noreply.github.com>
2020-01-24 11:44:50 -0800

    Update MIDI Reader.cpp

--- MIDI Reader.cpp
+++ MIDI Reader.cpp
@@ -6,33 +6,27 @@
 using namespace std;
 using namespace smf;
 
-int main(int argc, char** argv) {
-   Options options;
-   options.process(argc, argv);
+int main() {
    MidiFile midifile;
-   if (options.getArgCount() == 0) midifile.read(cin);
-   else midifile.read(options.getArg(1));
+   midifile.read("C:Users/micah/Downloads/la_campanella.mid");
    midifile.doTimeAnalysis();
    midifile.linkNotePairs();
 
    int tracks = midifile.getTrackCount();
-   cout << "TPQ: " << midifile.getTicksPerQuarterNote() << endl;
-   if (tracks > 1) cout << "TRACKS: " << tracks << endl;
-   for (int track=0; track<tracks; track++) {
-      if (tracks > 1) cout << "\nTrack " << track << endl;
-      cout << "Tick\tSeconds\tDur\tMessage" << endl;
-      for (int event=0; event<midifile[track].size(); event++) {
-         cout << dec << midifile[track][event].tick;
-         cout << '\t' << dec << midifile[track][event].seconds;
-         cout << '\t';
-         if (midifile[track][event].isNoteOn())
-            cout << midifile[track][event].getDurationInSeconds();
-         cout << '\t' << hex;
-         for (int i=0; i<midifile[track][event].size(); i++)
-            cout << (int)midifile[track][event][i] << ' ';
-         cout << endl;
+   cout << "TPQN: " << midifile.getTicksPerQuarterNote() << endl;
+   cout << "TRACKS: " << tracks << endl;
+   cout << "Tick\tSeconds\tDur\tMessage" << endl;
+   for (int event=0; event<midifile[track].size(); event++) {
+      cout << dec << midifile[track][event].tick;
+      cout << '\t' << dec << midifile[track][event].seconds;
+      cout << '\t';
+      if (midifile[track][event].isNoteOn())
+         cout << midifile[track][event].getDurationInSeconds();
+      cout << '\t' << hex;
+      for (int i=0; i<midifile[track][event].size(); i++)
+         cout << (int)midifile[track][event][i] << ' ';
+      cout << endl;
       }
    }
-
    return 0;
 }
