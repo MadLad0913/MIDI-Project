c63a2d524874a10c0129a89563e74466349967be
MadLad0913 <46514516+MadLad0913@users.noreply.github.com>
2020-02-19 19:24:37 -0800

    Update MIDI Reader.cpp

--- MIDI Reader.cpp
+++ MIDI Reader.cpp
@@ -7,26 +7,28 @@ using namespace std;
 using namespace smf;
 
 int main() {
-   MidiFile midifile;
-   midifile.read("C:Users\\micah\\Downloads\\la_campanella.mid");
-   midifile.doTimeAnalysis();
-   midifile.linkNotePairs();
-
-   int tracks = midifile.getTrackCount();
-   cout << "TPQN: " << midifile.getTicksPerQuarterNote() << endl;
-   cout << "TRACKS: " << tracks << endl;
-   cout << "Tick\tSeconds\tDur\tMessage" << endl;
-   for (int event=0; event<midifile[track].size(); event++) {
-      cout << dec << midifile[track][event].tick;
-      cout << '\t' << dec << midifile[track][event].seconds;
-      cout << '\t';
-      if (midifile[track][event].isNoteOn())
-         cout << midifile[track][event].getDurationInSeconds();
-      cout << '\t' << hex;
-      for (int i=0; i<midifile[track][event].size(); i++)
-         cout << (int)midifile[track][event][i] << ' ';
-      cout << endl;
-      }
-   }
-   return 0;
+	MidiFile midifile;
+	midifile.read("C:Users\\micah\\Downloads\\la_campanella.mid.mid");
+	cout << "TPQ: " << midifile.getTicksPerQuarterNote() << endl;
+	cout << "TRACKS: " << midifile.getTrackCount() << endl;
+	midifile.joinTracks();
+	// midifile.getTrackCount() will now return "1", but original
+	// track assignments can be seen in .track field of MidiEvent.
+	cout << "TICK    DELTA   TRACK   MIDI MESSAGE\n";
+	cout << "____________________________________\n";
+	MidiEvent* mev;
+	int deltatick;
+	for (int event = 0; event < midifile[0].size(); event++) {
+		mev = &midifile[0][event];
+		if (event == 0) deltatick = mev->tick;
+		else deltatick = mev->tick - midifile[0][event - 1].tick;
+		cout << dec << mev->tick;
+		cout << '\t' << deltatick;
+		cout << '\t' << mev->track;
+		cout << '\t' << hex;
+		for (int i = 0; i < mev->size(); i++)
+			cout << (int)(*mev)[i] << ' ';
+		cout << endl;
+	}
+	return 0;
 }
